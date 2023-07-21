366b47b2dad7c437da5fefa23d35c7c4cdcd787b
KwasiEvans <evansow2002@gmail.com>
2023-07-21 11:45:06 -1200

    created table fo registration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('assembly_name');
+            $table->string('assembly_id');
+            $table->string('district');
+            $table->string('area');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
