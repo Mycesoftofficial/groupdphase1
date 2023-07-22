1e2dd5c38bf84f7346e8d207139d882fb6485543
KwasiEvans <evansow2002@gmail.com>
2023-07-21 17:53:53 -1200

    intial work on dashboard

--- resources/views/layouts/auth_includes/scripts.blade.php
+++ resources/views/layouts/auth_includes/scripts.blade.php
@@ -5,6 +5,12 @@
 		<script src="{{asset('assets/vendors/scripts/layout-settings.js')}}"></script>
 		<script src="{{asset('assets/src/plugins/jquery-steps/jquery.steps.js')}}"></script>
 		<script src="{{asset('assets/vendors/scripts/steps-setting.js')}}"></script>
+		<script src="{{asset('assets/src/plugins/apexcharts/apexcharts.min.js')}}"></script>
+		<script src="{{asset('assets/src/plugins/datatables/js/jquery.dataTables.min.js')}}"></script>
+		<script src="{{asset('assets/src/plugins/datatables/js/dataTables.bootstrap4.min.js')}}"></script>
+		<script src="{{asset('assets/src/plugins/datatables/js/dataTables.responsive.min.js')}}"></script>
+		<script src="{{asset('assets/src/plugins/datatables/js/responsive.bootstrap4.min.js')}}"></script>
+		<script src="{{asset('assets/vendors/scripts/dashboard3.js')}}"></script>
 		<!-- Google Tag Manager (noscript) -->
 		<noscript
 			><iframe
